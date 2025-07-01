41a30f6d93a03d515844c7d7fa1a36756d25e449
rajavasanthan <rajavasanthan@gmail.com>
2025-07-01 19:28:22 +0530

    login added

---
+++ src/Home.jsx
@@ -0,0 +1,260 @@
+import axios from "axios";
+import { useFormik } from "formik";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+function Home() {
+  const [products, setProducts] = useState([]);
+  const [isEdit, setisEdit] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
+  const navigate = useNavigate()
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      price: "",
+      qty: "",
+    },
+    validate: (value) => {
+      let error = {};
+
+      if (value.title == "") {
+        error.title = "Please enter the title";
+      }
+
+      if (parseInt(value.price) < 0 || !value.price) {
+        error.price = "Please enter correct price";
+      }
+
+      if (parseInt(value.qty) < 0 || !value.qty) {
+        error.qty = "Please enter correct Quantity";
+      }
+
+      return error;
+    },
+    onSubmit: async (value) => {
+      if (isEdit) {
+        await axios.put(`${import.meta.env.VITE_API}/product/${currentId}`, value, {
+          headers: {
+            Authorization: window.localStorage.getItem("app_token"),
+          },
+        });
+        const products = await axios.get(
+          `${import.meta.env.VITE_API}/products`,
+          {
+            headers: {
+              Authorization: window.localStorage.getItem("app_token"),
+            },
+          }
+        );
+        setProducts(products.data);
+        setisEdit(false);
+        setCurrentId(null);
+        formik.resetForm();
+      } else {
+        await axios.post(`${import.meta.env.VITE_API}/product`, value, {
+          headers: {
+            Authorization: window.localStorage.getItem("app_token"),
+          },
+        });
+        const products = await axios.get(
+          `${import.meta.env.VITE_API}/products`,
+          {
+            headers: {
+              Authorization: window.localStorage.getItem("app_token"),
+            },
+          }
+        );
+        setProducts(products.data);
+        setProducts([...products, value]);
+        formik.resetForm();
+      }
+    },
+  });
+
+  let handleEdit = (_id) => {
+    console.log(_id);
+    let product = products.find((pro) => pro._id === _id);
+    if (product) {
+      formik.setValues({
+        title: product.title,
+        price: product.price,
+        qty: product.qty,
+      });
+      setisEdit(true);
+      setCurrentId(_id);
+    }
+  };
+
+  let handleFetch = async () => {
+    try {
+      const products = await axios.get(`${import.meta.env.VITE_API}/products`, {
+        headers: {
+          Authorization: window.localStorage.getItem("app_token"),
+        },
+      });
+      setProducts(products.data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  let handleDelete = async (_id) => {
+    try {
+      await axios.delete(`${import.meta.env.VITE_API}/product/${_id}`, {
+        headers: {
+          Authorization: window.localStorage.getItem("app_token"),
+        },
+      });
+      const products = await axios.get(`${import.meta.env.VITE_API}/products`, {
+        headers: {
+          Authorization: window.localStorage.getItem("app_token"),
+        },
+      });
+      setProducts(products.data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(import.meta.env.VITE_API);
+    handleFetch();
+  }, []);
+  return (
+    <div className="flex mx-auto">
+      <div className="w-1/2 p-5">
+        <h1 className="text-2xl">Add Product</h1>
+        <h3>
+            <button onClick={() => {
+                window.localStorage.removeItem("app_token")
+                navigate("/")
+            }}>Logout</button>
+        </h3>
+        <form class="max-w-lg mx-auto" onSubmit={formik.handleSubmit}>
+          <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium">
+              Product Title
+            </label>
+            <input
+              type="text"
+              name="title"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              id="email"
+              class="bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <span className="text-red-500">
+              {formik.touched.title &&
+                formik.errors.title &&
+                formik.errors.title}
+            </span>
+          </div>
+
+          <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium">
+              Product Price
+            </label>
+            <input
+              type="number"
+              name="price"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              id="email"
+              class="bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <span className="text-red-500">
+              {formik.touched.price &&
+                formik.errors.price &&
+                formik.errors.price}
+            </span>
+          </div>
+          <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium">
+              Quantity Avilable
+            </label>
+            <input
+              type="number"
+              name="qty"
+              value={formik.values.qty}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              id="email"
+              class="bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <span className="text-red-500">
+              {formik.touched.qty && formik.errors.qty && formik.errors.qty}
+            </span>
+          </div>
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="w-1/2">
+        <div class="relative overflow-x-auto">
+          <h1 className="text-2xl">Items</h1>
+          <table class="w-full text-sm text-left rtl:text-right  ">
+            <thead class="text-xs  uppercase bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Product name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Quantity
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => {
+                return (
+                  <tr class="bg-white border-b text-black">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium  whitespace-nowrap"
+                    >
+                      {product.title}
+                    </th>
+                    <td class="px-6 py-4">{product.price}</td>
+                    <td class="px-6 py-4">{product.qty}</td>
+                    <td class="px-6 py-4">
+                      <button
+                        className="cursor-pointer hover:text-gray-400"
+                        onClick={() => {
+                          handleEdit(product._id);
+                        }}
+                      >
+                        Edit
+                      </button>{" "}
+                      |{" "}
+                      <button
+                        onClick={() => {
+                          handleDelete(product._id);
+                        }}
+                        className="cursor-pointer hover:text-gray-400"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Register.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className="flex w-full justify-center items-center">
+        <div className="w-2xl">
+            <form>
+                <h1 className="text-2xl">Register</h1>
+            <label htmlFor="">Email</label>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
+            <label htmlFor="">Password</label>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
+            <label htmlFor="">Re-Enter Password</label>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
+            <button type="button" class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Register</button>
+
+        </form>
+        </div>
+    </div>
+  )
+}
+
+export default Register
