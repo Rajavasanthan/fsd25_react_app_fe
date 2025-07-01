6950cc9fc05c73bc17b727ff668802227a28ce7d
rajavasanthan <rajavasanthan@gmail.com>
2025-07-01 19:58:19 +0530

    color added

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useFormik } from "formik";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 function Login() {
     const navigate = useNavigate()
@@ -72,6 +72,9 @@ function Login() {
             type="submit"
             class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           />
+          <div>
+            Dont have Account? <Link to={"/register"} className="text-blue-600">Register</Link>
+          </div>
         </form>
       </div>
     </div>
